42e5a5d093f8cb18ca0d491f4866a623db10d497
kotvasili <kot_vasili@mail.ru>
2017-05-23 19:36:56 +0300

    1-0, started 1-1

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,5 +1,5 @@
 //=include lib/jquery-3.1.1.min.js
-//=include lib/slick.min.js
+//=include lib/flickity.pkgd.js
 //=include lib/jquery.matchHeight-min.js
 //=include lib/jquery-ui.min.js
 //=include lib/jquery.fullpage.js
